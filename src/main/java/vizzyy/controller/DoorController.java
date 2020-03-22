5fdf37e01fa91f0262e6ef0eb37dbc2fab55cb76
Vizzyy <barnabas.vizy@gmail.com>
2020-03-22 12:03:47 -0400

    Door status endpoints

--- src/main/java/vizzyy/controller/DoorController.java
+++ src/main/java/vizzyy/controller/DoorController.java
@@ -3,6 +3,8 @@ package vizzyy.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import vizzyy.service.LoggingService;
@@ -19,6 +21,8 @@ public class DoorController {
     @Autowired
     RestTemplate restTemplate;
 
+    private boolean isDoorOpen = false;
+
     private static String ddns = (String) S3ResourceService.loadFileFromS3("vizzyy", "credentials/ddns.url").toArray()[0];
 
     @RequestMapping(value = "/open")
@@ -26,6 +30,7 @@ public class DoorController {
         String entry = "User BLEEP BLOOP opened door at BLAH BLAH BLAH time.";
         loggingService.addEntry("Calling /door/open?entry=");
         String res = restTemplate.getForObject(ddns + ":9000/open?entry="+entry, String.class);
+        isDoorOpen = true;
         return res;
     }
 
@@ -34,7 +39,20 @@ public class DoorController {
         String entry = "User BLEEP BLOOP closed door at BLAH BLAH BLAH time.";
         loggingService.addEntry("Calling /door/close?entry=");
         String res = restTemplate.getForObject(ddns + ":9000/close?entry="+entry, String.class);
+        isDoorOpen = false;
         return res;
+    }
+
+    @RequestMapping(value = "/state", method = RequestMethod.POST)
+    public void setState(@RequestParam boolean state){
+        loggingService.addEntry("Set door state - door is opened: "+ state);
+        isDoorOpen = state;
+    }
+
+    @RequestMapping(value = "/state", method = RequestMethod.GET)
+    public boolean getState(){
+        loggingService.addEntry("Get door state - door is opened: "+isDoorOpen);
+        return isDoorOpen;
     }
 
 }
