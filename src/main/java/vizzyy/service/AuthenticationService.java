bc785b63ecb963d86d55e8dcc13c412138f8f1f1
Vizzyy <barnabas.vizy@gmail.com>
2020-04-05 17:24:49 -0400

    Various minor changes

--- src/main/java/vizzyy/service/AuthenticationService.java
+++ src/main/java/vizzyy/service/AuthenticationService.java
@@ -12,7 +12,7 @@ public class AuthenticationService {
 
     public static String getUserName(){
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (!(authentication instanceof AnonymousAuthenticationToken)) {
+        if (authentication != null && !(authentication instanceof AnonymousAuthenticationToken)) {
             String currentUserName = authentication.getName();
             return currentUserName;
         }
