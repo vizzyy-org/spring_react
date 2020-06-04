5fb6214b0fbe47e74d9eea828015a57b9d7f83cf
Vizzyy <barnabas.vizy@gmail.com>
2020-06-04 11:30:36 -0400

    Enable CSRF protection

--- src/main/java/vizzyy/config/SecurityConfiguration.java
+++ src/main/java/vizzyy/config/SecurityConfiguration.java
@@ -46,7 +46,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
                 .authorizeRequests().antMatchers("/actuator/health").permitAll()
                 .anyRequest().authenticated()
                 .and()
